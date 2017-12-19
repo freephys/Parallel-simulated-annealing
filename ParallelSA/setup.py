55e23d90980d777560234df51e75487323f138bb
ziyixi <ziyixi@mail.ustc.edu.cn>
2017-12-19 14:39:54 +0800

    build pip distribution

---
+++ ParallelSA/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='ParallelSA',
+    url='https://github.com/ziyixi/Parallel-simulated-annealing',
+    author='Ziyi Xi',
+    author_email='xiziyi2015@gmail.com',
+    version='1.0dev',
+    packages=['src',],
+    license='gpl-3.0',
+    long_description=open('README').read())
